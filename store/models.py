542606f65c1e4598c286caa82dad6ad21c0c6565
spacelinkers <josen221@gmail.com>
2024-09-21 22:56:41 +0600

    added product details page.

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 from category.models import Category
 
@@ -14,6 +15,9 @@ class Product(models.Model):
     modified_date = models.DateTimeField(auto_now=True)
     is_available = models.BooleanField(default=True)
     images = models.ImageField(upload_to='photos/products')
+
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
 
     def __str__(self):
         return self.product_name 
