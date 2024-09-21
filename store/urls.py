4aa94596e0694a5907f254dab3c23335e7337709
spacelinkers <josen221@gmail.com>
2024-09-21 20:11:04 +0600

    category wise store page.

---
+++ store/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.store, name='store'),
+    path('category/<slug:category_slug>/', views.store, name='products_by_category'),
+]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Product
+from category.models import Category
+
+
+def store(request, category_slug=None):
+    '''Get All products/category wise products'''
+    category = None
+    products = None
+
+    if category_slug != None:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.all().filter(category=category, is_available=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
+
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+
+    return render(request, 'store/store.html', context)
