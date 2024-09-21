5c892eced6482f54db39e6e9e80b90a7381e6eff
spacelinkers <josen221@gmail.com>
2024-09-21 17:15:41 +0600

    added store model and products in home page

--- store/models.py
+++ store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from category.models import Category
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(max_length=500, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    is_available = models.BooleanField(default=True)
+    images = models.ImageField(upload_to='photos/products')
+
+    def __str__(self):
+        return self.product_name 
