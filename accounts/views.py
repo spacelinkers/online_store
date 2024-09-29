28a4f42b9ff32196d9ac9eac04332fce33343eb5
spacelinkers <josen221@gmail.com>
2024-09-29 22:35:30 +0600

    added user login and logout.

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 
 from .forms import RegistrationForm
 from .models import Account
@@ -38,8 +39,24 @@ def register(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Ivalid login credentials")
+            return redirect('login')
+        
     return render(request, 'accounts/login.html')
 
 
+@login_required(login_url = 'login')
 def logout(request):
-    return 
+    auth.logout(request)
+    messages.success(request, 'You are logged out.')
+    return redirect('login') 
